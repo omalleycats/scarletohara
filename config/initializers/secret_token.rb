247edeb812a073425f8d51e4a0d3257469c65a23
Sarah O'Malley <sarahomalley@sarah-omalleys-macbook-3.local>
2014-01-25 19:54:46 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scarletohara::Application.config.secret_key_base = 'aa6dcad967dc08cbbd83999e44b86b5b3b93bdcd6967fd16452f8c1206f32d2018b927b21ebef7aae54ec0a664cf09f59154b8bc29a1be4d3fbc02d3b360ddf3'
